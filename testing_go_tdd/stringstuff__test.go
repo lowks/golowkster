89f9d7752b3c99ab4821a324d014bd2818bf0c3d
Low Kian Seong <lukman.low@censof.com>
2016-10-25 19:44:24 +0800

    re Added skip test and corrected previous test. Turns out needs to be camelcase

--- testing_go_tdd/stringstuff__test.go
+++ testing_go_tdd/stringstuff__test.go
@@ -10,10 +10,14 @@ func TestPrintStuff(t *testing.T) {
     }
 }
 
-func TestaddTwoNumbers(t *testing.T) {
+func TestAddTwoNumbers(t *testing.T) {
     if actual, expected := addTwoNumbers(5,7), 12; actual != expected {
         t.Errorf("Expected %s to Equal %s", actual, expected)
     } else {
         t.Log("Successfully tested")
     }
+}
+
+func TestSkipTest(t *testing.T) {
+    t.Skip("This is a skipped test")
 }
