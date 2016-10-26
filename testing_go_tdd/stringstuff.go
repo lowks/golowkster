f880fbf83901c22999cb1571bee5cc608a3ce0c9
Low Kian Seong <lukman.low@censof.com>
2016-10-26 10:35:08 +0800

    Adding filepath example

--- testing_go_tdd/stringstuff.go
+++ testing_go_tdd/stringstuff.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "path/filepath"
 
 func PrintSomeStuff(input string) string {
 	return fmt.Sprintf("hello %s", input)
@@ -8,4 +9,8 @@ func PrintSomeStuff(input string) string {
 
 func addTwoNumbers(x int, y int) int {
     return x + y
+}
+
+func JoinPath(first string, second string) string {
+    return fmt.Sprintf(filepath.Join(first, second))
 }
